9ea398b58396f3ba68199cd761c1c33d749c86c6
Vithu06 <159912921+Vithu06@users.noreply.github.com>
2024-02-13 14:57:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
